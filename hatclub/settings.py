7c3d68319362d8e9e9d399cbd0a858fe082d0fbb
The-Underground-Club <tanner.smiley321@gmail.com>
2022-10-12 10:30:53 -0500

    auto config

--- hatclub/settings.py
+++ hatclub/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -110,3 +111,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_on_heroku.settings(locals())
