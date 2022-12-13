82e81a1605bddd0fdc3c6092fbcaf993b47cfca9
The-Underground-Club <tanner.smiley321@gmail.com>
2022-12-13 15:01:35 -0600

    fixed deployment

--- hatclub/settings.py
+++ hatclub/settings.py
@@ -5,17 +5,11 @@ from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['zeldasman.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'zeldasman.pythonanywhere.com']
 
 
 # Application definition
@@ -107,6 +101,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, 'main/static/')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
